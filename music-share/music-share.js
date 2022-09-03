b7538b460590b455744b3bb2c514da5c7646128c
LightFly <lightfly25@gmail.com>
2022-09-03 17:27:28 +0800

    update

--- music-share/music-share.js
+++ music-share/music-share.js
@@ -23,7 +23,7 @@ const CONFIGINFO = {
 const PATH = {
     DIR: "./plugins/music-share/",
     INSTALL: "./plugins/AddonsHelper/",
-    PLUGINS:"./plugins/music-share/plugins/",
+    PLUGINS: "./plugins/music-share/plugins/",
     LIB: {
         DIR: "./plugins/lib/ffmpeg"
     },
@@ -31,15 +31,14 @@ const PATH = {
     INDEX: {
         DIR: "./plugins/music-share/index/",
         CONFIG: "./plugins/music-share/index/config.json",
-        MUSIC_LIST:"./plugins/music-share/index/musicList.json",
+        MUSIC_LIST: "./plugins/music-share/index/musicList.json",
         MUSIC_PACK: "./plugins/music-share/index/pack/",
         MUSIC_CONTENT: "./plugins/music-share/index/pack/content.json",
-        MUSIC_LOCK: "./plugins/music-share/index/pack/lock.json"
+        //MUSIC_LOCK: "./plugins/music-share/index/pack/lock.json"
     },
-
     CACHE: {
         DIR: "./plugins/music-share/cache/",
-        BUILD:"./plugins/music-share/cache/build/",
+        BUILD: "./plugins/music-share/cache/build/",
         PACK: "./plugins/music-share/cache/pack/"
     }
 }
@@ -50,8 +49,9 @@ ll.registerPlugin(INFO.name, INFO.intro, INFO.version, INFO.other)
 
 //"deleteOldSound":true,
 const GLConfig = new JsonConfigFile(PATH.INDEX.CONFIG,
-`{
+    `{
     "soundPostfix" : "[mp3|flac]",
+    "fmtArg":"-acodec libvorbis "
     "plugins":[
         {
             "index":"userGUI.js",
@@ -60,22 +60,31 @@ const GLConfig = new JsonConfigFile(PATH.INDEX.CONFIG,
         {
             "index":"lrcLoad.js",
             "enable":false
+        },
+        {
+            "index":"opMGR.js",
+            "enable":false
         }
     ]
 }`)
-const GLMusicList = new JsonConfigFile(PATH.INDEX.MUSIC_LIST,"{}")
+const GLMusicList = new JsonConfigFile(PATH.INDEX.MUSIC_LIST, "{}")
 
 
 
 function packMain() {
-    packCheck()
-
+    let Jcontent = new JsonConfigFile(PATH.INDEX.MUSIC_CONTENT, `
+    {
+        "_version_":${CONFIGINFO.VERSION},
+        "pack":{},
+        "reLoadList":[]
+    }`)
+    packCheck(Jcontent)
+    updatePackList(Jcontent)
 }
 
 
 //检查content是否完整 转换其他格式为ogg格式 列出pack音乐列表
-function packCheck() {
-    let Jcontent = new JsonConfigFile(PATH.INDEX.MUSIC_CONTENT,"[]")
+function packCheck(Jcontent) {
     let packlist = Jcontent.get("pack")
     for (let i in packlist) {
         //需要手动更新
@@ -86,7 +95,7 @@ function packCheck() {
 
         //version
         logger.debug(packlist[i]["check"]["version"].toString())
-        // why toStr [0,0,0] != [0,0,0]
+        // why toStr -> [0,0,0] != [0,0,0]
         if (packlist[i]["check"]["version"].toString() == "0,0,0" || packlist[i]["check"]["version"] == undefined) {
             //logger.debug(versionAdd(packlist[i]["check"]["version"]))
             packlist[i]["check"]["version"] = versionAdd(packlist[i]["check"]["version"])
@@ -117,55 +126,71 @@ function packCheck() {
         if (File.exists(packdir + "/index/checklist.json")) {
             Jchecklist.set("_version_", CONFIGINFO.VERSION)
             Jchecklist.set("list", {})
-        } else if(needReload ==true){
+        } else if (needReload == true) {
             //忽略需要更新的包
-        }else{
+        } else {
             let checklist = Jchecklist.get("list")
             let nowlist = regA_Z(musiclist, "[ogg]")[0]
-            if (!(checklist.length!=nowlist.length)){
+            if (!(checklist.length != nowlist.length)) {
                 for (i in checklist) {
                     let haslist = false
                     for (let v = 0; v < nowlist; v++) {
-                        if(checklist==nowlist[v]){
-                            haslist =true
+                        if (checklist == nowlist[v]) {
+                            haslist = true
                             break
                         }
                     }
-                    if (haslist = false){
+                    if (haslist = false) {
                         needReload = true
                     }
                 }
-            }else{
-                needReload =true
+            } else {
+                needReload = true
             }
         }
+        Jchecklist.close()
 
         //将当前包是否需要更新 写入content
-        if(needReload == true){
-            packlist[i]["reLoad"] == true
+        if (needReload == true) {
+            let arry = Jcontent.get("reLoadList")
+            arry.push(i)
+            logger.debug("update dir :", arry)
+            Jcontent.set("reLoadList", arry)
+
         }
 
-        logger.debug("check over :",packlist)
-        Jcontent.set("pack",packlist)
-        Jcontent.close()
+        logger.debug("check over :", packlist)
+        Jcontent.set("pack", packlist)
     }
-
+    logger.debug("check over: ", Jcontent.get("reLoadList"))
 }
 
+function updatePackList(Jcontent,) {
+    //需要更新的包
+    let updatelist = Jcontent.get("reLoadList")
 
-//检查是否有新歌或者删除旧歌
-// function packCheck() {
-//     let Jcontent = new JsonConfigFile(PATH.INDEX.MUSIC_CONTENT, [])
-//     let content = Jcontent.read()
-//     content = data.parseJson(content)
-//     for (let i = 0;i<content.length;i++){
 
-//         //忽略要重载的包
-//         if(content[i]["reLoad"] == true || File.exists()){
-//             continue
-//         }
-//     }
-// }
+    for (let i = 0; i < updatelist.length; i++) {
+        let packlist = File.getFilesList(PATH.INDEX.MUSIC_PACK + updatelist[i] + "/index/")
+        let cutPostfix = new RegExp("[.][ogg]")
+        packlist = regA_Z(packCheck,"[ogg]")
+
+        //传入cache临时目录 
+
+        //去掉ogg 的文件名
+        for (let i = 0;i<packlist.length;i++){
+            packlist[i].replace(cutPostfix,"")
+        }
+
+        //打包到AddonsHelper
+
+        //在musicList park 当前包代号：音乐列表
+
+        //在musicList main 将全部包整合 写入
+    }
+
+
+}
 
 function radomUUID() {
     let UUID = ""
@@ -185,12 +210,12 @@ function versionAdd(version) {
     for (let i = 2; i > 0; i--) {
         if (version[i] < 9 && i != 0) {
             version[i] += 1
-            logger.debug("version 1 ",version)
+            logger.debug("version 1 ", version)
             break
         } else if (version[i] >= 9 && i != 0) {
             version[i] = 0
             version[i - 1] += 1
-            logger.debug("version 2 ",version)
+            logger.debug("version 2 ", version)
             break
         } else if (version[0] == 9 && version[1] == 9) {
             logger.warn("版本即将达到最大值")
@@ -223,21 +248,21 @@ function toOGG(path, fileArry) {
     for (let i = 0; i < fileArry.length; i++) {
         let outname = fileArry[i].replace(reg, ".ogg")
         system.newProcess("./plugins/lib/ffmpeg/ffmpeg.exe -i " +
-            path + fileArry[i] + "-acodec libvorbis " + path + outname
+            path + fileArry[i] + GLConfig.get("fmtArg") + path + outname
         )
         logger.info("已转换：", outname)
     }
 }
 
 
-mc.listen("onServerStarted",function(){
-    let LMC =  mc.newCommand("lfmusic", "音乐控制 导入 新建 music-share插件命令", PermType.GameMasters)
+mc.listen("onServerStarted", function () {
+    let LMC = mc.newCommand("lfmusic", "音乐控制 导入 新建 music-share插件命令", PermType.GameMasters)
     LMC.setAlias("lmc")
-    LMC.setEnum("OPmgr",["build"])
-    LMC.mandatory("OParg",ParamType.Enum,"OPmgr")
-    LMC.overload(["OPmgr"])
-    LMC.setCallback(function(_,ori,_,res){
-        switch(res.OParg){
+    LMC.setEnum("build", ["build"])
+    LMC.mandatory("OParg", ParamType.Enum, "build")
+    LMC.overload(["build"])
+    LMC.setCallback(function (_, ori, _, res) {
+        switch (res.OParg) {
             case "build":
                 packMain(ori)
                 return
